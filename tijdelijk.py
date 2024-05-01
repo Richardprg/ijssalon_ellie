ea834cafd2d4b84b134ee33a4eb542dfb4a2ea29
Richard Saarberg <r-s-b@hotmail.com>
2024-05-01 13:27:10 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -8,5 +8,8 @@ reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts €
 reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = (reclame_tekst2.upper())
 reclame_tekst4 = (reclame_tekst3.split(" "))
-for el in reclame_tekst4:    
-    print(el.lower())
+for el in reclame_tekst4:
+    if len(el) > 4:
+        print(el.upper())
+    else:
+        print(el.lower())
